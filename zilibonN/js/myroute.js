61b8d6b029c6dabdeb888aafa069bbe2745ec957
wsxwater <675162796@qq.com>
2018-04-25 09:53:52 +0800

    修改首页第2次

---
+++ zilibonN/js/myroute.js
@@ -0,0 +1,16 @@
+$(function(){
+	
+	if($('[is="head"]')){
+	    $('[is="head"]').load('template/head.html');
+	}
+	if($('[is="nav"]')){
+	    $('[is="nav"]').load('template/nav.html');
+	}
+	if($('[is="foot"]')){
+	    $('[is="foot"]').load('template/foot.html');
+	}
+	if($('[is="classify"]')){
+	    $('[is="classify"]').load('template/classify.html');
+	}
+	
+})
