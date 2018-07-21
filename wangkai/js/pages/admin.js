0d9855cfb3a493596918582a6d367a0f2ecc3f2c
wsxwater <675162796@qq.com>
2018-07-21 17:03:52 +0800

    modify list.css

--- wangkai/js/pages/admin.js
+++ wangkai/js/pages/admin.js
@@ -191,6 +191,8 @@ $(function () {
         for (var i = 0; i < links.length; i++) {
             var link_url=links.eq(i).children().attr('href');
             var cur_url =window.location.href;
+            // console.log(cur_url);
+            // console.log(link_url);
             if (cur_url.indexOf(link_url)!=-1) {
                 links.eq(i).children().addClass('active')
             }
